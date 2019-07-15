0fd90d04c2c46e3fac2fbadcba15a7a85dd627f1
insolor <insolor@gmail.com>
2019-07-15 13:55:10 +0300

    Add service.py, requirements.txt

---
+++ service.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def root():
+    print(request)
+    return 'OK'
+
+
+@app.route('/auth', methods=['POST'])
+def auth():
+    print(request.data)
+    print(request.get_json())
+    return 'OK'
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
