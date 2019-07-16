128d9a9b09ae811bf02539348a30dcf0a850b82b
insolor <insolor@gmail.com>
2019-07-16 11:20:20 +0300

    Implement authorisation

--- service.py
+++ service.py
@@ -1,19 +1,53 @@
 #!/usr/bin/env python3
-from flask import Flask, request
+from flask import Flask, request, abort
+import uuid
+
 app = Flask(__name__)
+
+class User:
+    def __init__(self, password, is_admin=False, cart=None):
+        self.__password = password
+        self.is_admin = is_admin
+        self.cart = cart or list()
+    
+    def check_password(self, password):
+        return self.__password == password
+
+
+users = {
+    "admin": User("", True),
+    "user": User(""),
+}
+
+
+sessions = dict()
 
 
 @app.route('/', methods=['GET'])
 def root():
-    print(request)
     return 'OK'
 
 
 @app.route('/auth', methods=['POST'])
 def auth():
-    print(request.data)
-    print(request.get_json())
-    return 'OK'
+    data =  request.get_json()
+
+    if 'username' not in data or 'password' not in data:
+        abort(400)
+
+    username = data['username']
+    password = data['password']
+    if username not in users:
+        abort(403)
+    
+    user = users[username]
+    if not user.check_password(password):
+        abort(403)
+
+    token = uuid.uuid4()
+    sessions[token] = user
+
+    return str(token)
 
 
 if __name__ == "__main__":
