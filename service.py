789e4d6d300954ce70880d3a9fcc06ab1628f5c7
insolor <insolor@gmail.com>
2019-07-24 12:46:20 +0300

    Implement update_cart_data method

--- service.py
+++ service.py
@@ -155,6 +155,20 @@ def add_instrument_to_cart():
     return 'OK'
 
 
+@app.route('/cart/my/data', methods=['PUT'])
+def update_cart_data():
+    user = check_token(request)
+    if 'promocode' not in request.args or '' not in request.args:
+        abort(400)
+
+    cart = carts[user]  # type: Cart
+
+    cart.promocode = request.args['promocode']
+    cart.days = int(request.args['days'])
+
+    return 'OK'
+
+
 @app.route('/cart/my', methods=['DELETE'])
 def remove_from_cart():
     user = check_token(request)
